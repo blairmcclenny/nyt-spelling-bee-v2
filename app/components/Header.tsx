0829fcb8c8b25c83265048820559d59e151da6ef
Blair M McClenny <blairmmcclenny@Mac.lan>
2025-08-21 22:24:56 -0700

    style header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,13 @@
 export default function Header() {
   return (
-    <header className="pt-6">
-      <div className="pz-row pz-game-title-bar flex flex-wrap items-stretch min-h-px flex-[1_1_0%] m-auto max-w-7xl p-6">
-        <div className="pz-module">
+    <header className="pt-6 border-b border-b-stroke-tertiary">
+      <div className="flex flex-wrap items-stretch min-h-px flex-[1_1_0%] m-auto max-w-7xl p-6">
+        <div className="flex-[1_1_auto] m-3">
           <h2>
-            <em className="pz-game-title font-serif font-bold text-[37px] lg:text-[42px] mr-4 not-italic">
+            <em className="font-serif font-bold text-[37px] lg:text-[42px] mr-4 not-italic">
               Spelling Bee
             </em>
-            <span className="pz-game-date text-[25px] lg:text-[28px] inline-block font-light">
+            <span className="text-[25px] lg:text-[28px] inline-block font-light">
               August 21, 2025
             </span>
           </h2>
