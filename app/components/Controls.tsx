32cfe89b1e986607bef7fa0d4c903c267e081cb2
Blair M McClenny <blairmmcclenny@Mac.lan>
2025-08-25 16:50:37 -0700

    style message box

--- app/components/Controls.tsx
+++ app/components/Controls.tsx
@@ -7,7 +7,7 @@ export default function Controls() {
   return (
     <div className="flex flex-1 relative justify-center items-center">
       <div className="sb-controls relative max-h-full flex flex-col w-[80vw] max-w-[290px] z-[3] items-center pb-[calc(--spacing(1)*2.5)] xs:pb-[calc(--spacing(5)*2.25)]">
-        <MessageBox message="Not in word list" type="error" points={7} />
+        <MessageBox message="Nice!" type="pangram" points={7} />
         <HiveInput input="guess" />
         <Hive letters={["a", "b", "c", "d", "e", "f", "g"]} />
         <HiveActions />
