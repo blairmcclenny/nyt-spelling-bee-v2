a1782a7d7a10d4ba9f0b98f6064401749eec9e8e
Blair M McClenny <blairmmcclenny@Mac.lan>
2025-09-03 11:23:40 -0700

    animate message box

--- app/contexts/GameContext.tsx
+++ app/contexts/GameContext.tsx
@@ -4,6 +4,12 @@ import { createContext, useContext, useEffect, useState } from "react"
 import { games } from "@/app/data/mock"
 
 type ShuffleState = "idle" | "fadingOut" | "shuffling" | "fadingIn"
+type Message = {
+  value: string
+  points?: number
+  isPangram?: boolean
+  isError?: boolean
+} | null
 
 const GameContext = createContext<{
   centerLetter: string
@@ -17,8 +23,8 @@ const GameContext = createContext<{
   setFoundWords: React.Dispatch<React.SetStateAction<string[]>>
   shuffleState: ShuffleState
   setShuffleState: React.Dispatch<React.SetStateAction<ShuffleState>>
-  message: string | null
-  setMessage: React.Dispatch<React.SetStateAction<string | null>>
+  message: Message | null
+  setMessage: React.Dispatch<React.SetStateAction<Message | null>>
   score: number
   setScore: React.Dispatch<React.SetStateAction<number>>
 }>({
@@ -44,7 +50,7 @@ export function GameProvider({ children }: { children: React.ReactNode }) {
   const [input, setInput] = useState<string[]>([])
   const [foundWords, setFoundWords] = useState<string[]>([])
   const [shuffleState, setShuffleState] = useState<ShuffleState>("idle")
-  const [message, setMessage] = useState<string | null>(null)
+  const [message, setMessage] = useState<Message | null>(null)
   const [score, setScore] = useState<number>(0)
 
   useEffect(() => {
